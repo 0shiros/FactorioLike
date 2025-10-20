6661a1886b7379fe39ff9b40f99a482e2182db3f
0shiros <oli2014@gmx.fr>
2025-10-20 13:55:47 +0200

    Add camera drag

---
+++ Assets/Scripts/Camera/CameraDrag.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraDrag : MonoBehaviour
+{
+  [Header("Settings")]
+  private Vector3 origin;
+  private Vector3 difference;
+  private bool isDragging = false;
+  [SerializeField] private float dragSpeed = 1;
+  
+  [Header("References")]
+  private Camera mainCamera;
+
+  private Vector3 mousePosition => mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+  private void Awake()
+  {
+    mainCamera = Camera.main;
+  }
+
+  public void OnDrag(InputAction.CallbackContext context)
+  {
+    if (context.started)
+    {
+      origin = mousePosition;
+    }
+    isDragging = context.started || context.performed;
+  }
+
+  private void LateUpdate()
+  {
+    if(!isDragging) return;
+    difference = mousePosition - transform.position;
+    transform.position = origin - difference * dragSpeed;
+  }
+}
