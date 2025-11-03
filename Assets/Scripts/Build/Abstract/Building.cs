cca631f332dfc4643ffe0bc01b5eb46d5d355a6a
0shiros <oli2014@gmx.fr>
2025-11-03 17:13:29 +0100

    Add resfresh building detected + add logic to all building

--- Assets/Scripts/Build/Abstract/Building.cs
+++ Assets/Scripts/Build/Abstract/Building.cs
@@ -23,13 +23,20 @@ public abstract class Building : MonoBehaviour
     public List<ResourceAndAmount> resourcesStored;
     public float timeElapsed;
     public Building buildingDetected;
+    private float transferProgress = 0f;
     
     protected void Start()
     { 
         trans = transform;
-        DetectBuildingAround();
+        DetectBuilding();
+        RefreshBuildingAround();
     }
-    
+
+    protected virtual void Update()
+    {
+        if (buildingDetected == null) return;
+        MakeTheTransfert();
+    }
 
     public virtual void InitializeBuilding(BuildingData data, int order, Tilemap tilemapResource)
     {
@@ -54,7 +61,7 @@ public abstract class Building : MonoBehaviour
         }
     }
 
-    protected virtual void DetectBuildingAround()
+    public void DetectBuilding()
     {
         RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Vector2.down, 1f);
         foreach (RaycastHit2D hit in hits)
@@ -71,12 +78,90 @@ public abstract class Building : MonoBehaviour
         }
     }
 
+    public void RefreshBuildingAround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(trans.position, 1f);
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider != null && collider.gameObject != gameObject)
+            {
+                Building building = collider.GetComponent<Building>();
+
+                if (building.buildingDetected == null || building.buildingDetected != buildingDetected)
+                {
+                    building.DetectBuilding();
+                }
+            }
+        }
+    }
+    
+    protected void MakeTheTransfert()
+    {
+        if (resourcesStored.Count > 0) 
+        {
+            int transferAmount = CalculateTransferAmount();
+            if (transferAmount > 0)
+            {
+                ResourceAndAmount resourceToTransfer = resourcesStored[0];
+                TransferResourceToBuilding(buildingDetected, resourceToTransfer, transferAmount);
+            }
+        }
+    }
+
+    private void TransferResourceToBuilding(Building targetBuilding, ResourceAndAmount resource, int amount)
+    {
+        bool resourceFound = false;
+        foreach (ResourceAndAmount storedResource in targetBuilding.resourcesStored)
+        {
+            if (storedResource.resource == resource.resource)
+            {
+                storedResource.quantity += amount;
+                resourceFound = true;
+                break;
+            }
+        }
+
+        if (!resourceFound)
+        {
+            targetBuilding.resourcesStored.Add(new ResourceAndAmount(resource.resource, amount));
+        }
+
+        resource.quantity -= amount;
+    }
+
+    private int CalculateTransferAmount()
+    {
+        if (resourcesStored.Count == 0 || resourcesStored[0].quantity <= 0)
+        {
+            transferProgress = 0f; 
+            return 0;
+        }
+
+        float maxTransferPerSecond = (float)quantityResourcesPerCycle / cycleTime;
+
+        transferProgress += maxTransferPerSecond * Time.deltaTime;
+
+        int availableAmount = resourcesStored[0].quantity;
+        int amountToTransfer = Mathf.Min(Mathf.FloorToInt(transferProgress), availableAmount);
+
+        if (amountToTransfer > 0)
+            transferProgress -= amountToTransfer;
+        
+        if (resourcesStored[0].quantity - amountToTransfer <= 0)
+            transferProgress = 0f;
+
+        return amountToTransfer;
+    }
+
     private void OnDrawGizmos()
     {
         if (buildingType != BuildingType.Stock)
         {
             Gizmos.color = Color.red;
             Gizmos.DrawLine(trans.position, trans.position + Vector3.down);
+            
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(trans.position, 1f);
         }
     }
     
--- Assets/Scripts/Build/Buildings/TransportBuilding.cs
+++ Assets/Scripts/Build/Buildings/TransportBuilding.cs
@@ -3,69 +3,9 @@ using UnityEngine;
 
 public class TransportBuilding : Building
 {
-    private float transferProgress = 0f;
-    
-    private void Update()
+    protected override void Update()
     {
-        MakeTheTransfert();
-    }
-
-    private void MakeTheTransfert()
-    {
-        if (resourcesStored.Count > 0) 
-        {
-            int transferAmount = CalculateTransferAmount();
-            if (transferAmount > 0)
-            {
-                ResourceAndAmount resourceToTransfer = resourcesStored[0];
-                TransferResourceToBuilding(buildingDetected, resourceToTransfer, transferAmount);
-            }
-        }
-    }
-
-    private void TransferResourceToBuilding(Building targetBuilding, ResourceAndAmount resource, int amount)
-    {
-        bool resourceFound = false;
-        foreach (ResourceAndAmount storedResource in targetBuilding.resourcesStored)
-        {
-            if (storedResource.resource == resource.resource)
-            {
-                storedResource.quantity += amount;
-                resourceFound = true;
-                break;
-            }
-        }
-
-        if (!resourceFound)
-        {
-            targetBuilding.resourcesStored.Add(new ResourceAndAmount(resource.resource, amount));
-        }
-
-        resource.quantity -= amount;
-    }
-
-    private int CalculateTransferAmount()
-    {
-        if (resourcesStored.Count == 0 || resourcesStored[0].quantity <= 0)
-        {
-            transferProgress = 0f; 
-            return 0;
-        }
-
-        float maxTransferPerSecond = (float)quantityResourcesPerCycle / cycleTime;
-
-        transferProgress += maxTransferPerSecond * Time.deltaTime;
-
-        int availableAmount = resourcesStored[0].quantity;
-        int amountToTransfer = Mathf.Min(Mathf.FloorToInt(transferProgress), availableAmount);
-
-        if (amountToTransfer > 0)
-            transferProgress -= amountToTransfer;
-        
-        if (resourcesStored[0].quantity - amountToTransfer <= 0)
-            transferProgress = 0f;
-
-        return amountToTransfer;
+        base.Update();
     }
 
     private void ClearList()
